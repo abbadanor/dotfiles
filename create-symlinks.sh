9107554a445543fd8fbade60174d9e827e3e72a5
Adam Nord <adam.nord@abbgymnasiet.se>
2022-04-05 19:57:10 +0200

    added symlink script draft

---
+++ create-symlinks.sh
@@ -0,0 +1,21 @@
+!#/bin/bash
+
+# TODO: create script functionality
+
+ln -s ~/.dotfiles/rofi/config.rasi $XDG_CONFIG_HOME/rofi/
+ln -s ~/.dotfiles/rofi/themes/power.rasi $XDG_CONFIG_HOME/rofi/themes
+ln -s ~/.dotfiles/ncmpcpp/bindings/ $XDG_CONFIG_HOME/ncmpcpp
+ln -s ~/.dotfiles/ncmpcpp/config $XDG_CONFIG_HOME/ncmpcpp
+ln -s ~/.dotfiles/bspwm/bspwmrc $XDG_CONFIG_HOME/bspwm
+ln -s ~/.dotfiles/bspwm/terminals $XDG_CONFIG_HOME/bspwm
+ln -s ~/.dotfiles/lvim/config.lua $XDG_CONFIG_HOME/lvim
+ln -s ~/.dotfiles/feh/buttons $XDG_CONFIG_HOME/feh
+ln -s ~/.dotfiles/feh/keys $XDG_CONFIG_HOME/feh
+ln -s ~/.dotfiles/mpd/mpd.conf $XDG_CONFIG_HOME/mpd
+ln -s ~/.dotfiles/fontconfig/fonts.conf $XDG_CONFIG_HOME/fontconfig
+ln -s ~/.dotfiles/picom/picom.conf $XDG_CONFIG_HOME/picom
+ln -s ~/.dotfiles/sxhkd/sxhkdrc $XDG_CONFIG_HOME/sxhkdrc
+ln -s ~/.dotfiles/fish/config.fish $XDG_CONFIG_HOME/fish
+ln -s ~/.dotfiles/fish/functions/fish_prompt.fish $XDG_CONFIG_HOME/fish/functions
+ln -s ~/.dotfiles/polybar/config $XDG_CONFIG_HOME/polybar
+ln -s ~/.dotfiles/polybar/launch.sh $XDG_CONFIG_HOME/polybar
