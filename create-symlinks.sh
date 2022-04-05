f002256fc13d8f9ebb1b43293d9d595662e989f7
Adam Nord <adam.nord@abbgymnasiet.se>
2022-04-05 21:29:50 +0200

    added stuff

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -2,6 +2,13 @@
 
 # TODO: create script functionality
 
+ln -s ~/.dotfiles/bin $HOME/.local/bin 
+ln -s ~/.dotfiles/.profile $HOME
+ln -s ~/.dotfiles/.xbindkeysrc $HOME
+ln -s ~/.dotfiles/.xinitrc $HOME
+ln -s ~/.dotfiles/.lesskey $HOME
+ln -s ~/.dotfiles/.Xresources $HOME
+ln -s ~/.dotfiles/mimeapps.list $XDG_CONFIG_HOME
 ln -s ~/.dotfiles/rofi/config.rasi $XDG_CONFIG_HOME/rofi/
 ln -s ~/.dotfiles/rofi/themes/power.rasi $XDG_CONFIG_HOME/rofi/themes
 ln -s ~/.dotfiles/ncmpcpp/bindings/ $XDG_CONFIG_HOME/ncmpcpp
@@ -17,5 +24,6 @@ ln -s ~/.dotfiles/picom/picom.conf $XDG_CONFIG_HOME/picom
 ln -s ~/.dotfiles/sxhkd/sxhkdrc $XDG_CONFIG_HOME/sxhkdrc
 ln -s ~/.dotfiles/fish/config.fish $XDG_CONFIG_HOME/fish
 ln -s ~/.dotfiles/fish/functions/fish_prompt.fish $XDG_CONFIG_HOME/fish/functions
-ln -s ~/.dotfiles/polybar/config $XDG_CONFIG_HOME/polybar
+ln -s ~/.dotfiles/polybar/config.ini $XDG_CONFIG_HOME/polybar
 ln -s ~/.dotfiles/polybar/launch.sh $XDG_CONFIG_HOME/polybar
+sudo ln -s ~/.dotfiles/sv /usr/share/X11/xkb/symbols
